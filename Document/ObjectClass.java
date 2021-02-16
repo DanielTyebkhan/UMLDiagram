557079448537dca0ad4056f8ac0673b876efabaa
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-02-16 18:24:26 -0500

    Changed arrays to lists

--- Document/ObjectClass.java
+++ Document/ObjectClass.java
@@ -1,68 +1,86 @@
-package filemanagement;
+package Document;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class ObjectClass {
 	private String name;
-	private String[] instancevariables;
-	private String[] stereotypes;
-	private String[] methods;
+	private ArrayList<String> instanceVariables;
+	private ArrayList<String> stereotypes;
+	private ArrayList<String> methods;
 	private int[] pdata;
-	private String[] children;
+	private ArrayList<ObjectClass> children;
 	
-	public ObjectClass(String nm, int[] data) {
-		name=nm;
-		instancevariables= new String[] {};
-		stereotypes=new String[] {};
-		methods=new String[] {};
-		pdata=data;
-		children=new String[] {};
+	public ObjectClass(String nm, int[] position) {
+		name = nm;
+        instanceVariables = new ArrayList<String>();
+        stereotypes = new ArrayList<String>();
+        methods = new ArrayList<String>();
+        children = new ArrayList<ObjectClass>();
+        pdata = position;
 	}
 
 	public void setName(String nm) {
-		name=nm;
+		name = nm;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setInstanceVariables(String[] instancev) {
-		instancevariables=instancev;
+	public void addInstanceVariable(String variable) {
+        instanceVariables.add(variable);
+    }
+
+    public void setInstanceVariables(List<String> vars) {
+        instanceVariables = new ArrayList<String>(vars);
+    }
+
+	public List<String> getInstanceVariables() {
+		return new ArrayList<String>(instanceVariables);
 	}
 
-	public String[] getInstanceVariables() {
-		return instancevariables;
+	public void addStereotype(String stereotype) {
+        stereotypes.add(stereotype);
 	}
 
-	public void setStereotypes(String[] stereo) {
-		stereotypes=stereo;
+    public void setStereotypes(List<String> stereotypes) {
+        stereotypes = new ArrayList<String>(stereotypes);
+    }
+    
+	public List<String> getStereotypes() {
+		return new ArrayList<String>(stereotypes);
 	}
 
-	public String[] getStereotypes() {
-		return stereotypes;
+	public void addMethod(String method) {
+        methods.add(method);
 	}
 
-	public void setMethods(String[] metho) {
-		methods=metho;
-	}
+    public void setMethods(List<String> methods) {
+        methods = new ArrayList<String>(methods);
+    }
 
-	public String[] getMethods() {
-		return methods;
+	public List<String> getMethods() {
+        return new ArrayList<String>(methods);
 	}
 
 	public void setPosition(int[] data) {
-		pdata=data;
+		pdata = data;
 	}
 
 	public int[] getPosition() {
 		return pdata;
 	}
 
-	public void setChildren(String[] data) {
-		children=data;
+	public void setChildren(List<ObjectClass> children) {
+		children = children;
 	}
 
-	public String[] getChildren() {
-		return children;
+    public void addChild(ObjectClass child) {
+        children.add(child);
+    }
+
+	public List<ObjectClass> getChildren() {
+		return new ArrayList<ObjectClass>(children);
 	}
 }
