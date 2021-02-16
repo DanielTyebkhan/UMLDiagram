d7e347adfbb7253be4a7349a1b7de7e2e77cee23
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-02-16 18:02:25 -0500

    Moved ClassObject to Document package

--- Document/ObjectClass.java
+++ Document/ObjectClass.java
@@ -1,5 +1,6 @@
 package filemanagement;
 
+
 public class ObjectClass {
 	private String name;
 	private String[] instancevariables;
@@ -16,39 +17,51 @@ public class ObjectClass {
 		pdata=data;
 		children=new String[] {};
 	}
+
 	public void setName(String nm) {
 		name=nm;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setInstanceVariables(String[] instancev) {
 		instancevariables=instancev;
 	}
+
 	public String[] getInstanceVariables() {
 		return instancevariables;
 	}
+
 	public void setStereotypes(String[] stereo) {
 		stereotypes=stereo;
 	}
+
 	public String[] getStereotypes() {
 		return stereotypes;
 	}
+
 	public void setMethods(String[] metho) {
 		methods=metho;
 	}
+
 	public String[] getMethods() {
 		return methods;
 	}
+
 	public void setPosition(int[] data) {
 		pdata=data;
 	}
+
 	public int[] getPosition() {
 		return pdata;
 	}
+
 	public void setChildren(String[] data) {
 		children=data;
 	}
+
 	public String[] getChildren() {
 		return children;
 	}
