a17259e1f7c67072e1021c681066d151e65ebc71
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-02-16 18:42:00 -0500

    made Storage a subject

--- Document/Storage.java
+++ Document/Storage.java
@@ -1,2 +1,19 @@
+package Document;
+
+import java.util.List;
+import java.util.ArrayList;
+import Document.ObjectClass;
+
 public class Storage {
+    private ArrayList<ObjectClass> objects;
+    private ArrayList<Observer> observers;
+
+    public void notifyObservers() {
+        for (Observer observer : observers)
+            observer.update();
+    }
+
+    public void AddObject(ObjectClass object) {
+        objects.add(object);
+    }
 }
