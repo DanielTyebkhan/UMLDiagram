3173a8222dfdf564a8d6015251056a9fd54d2b04
Lawson Wheatley <wheatlel@union.edu>
2021-02-17 18:18:05 -0500

    Added Filemanagement

---
+++ src/main/java/Filemanagement/DataSerializer.java
@@ -0,0 +1,38 @@
+package Filemanagement;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.lang.ClassNotFoundException;
+import Document.Storage;
+
+public class DataSerializer {
+	public DataSerializer() {
+		
+	}
+	public void SerializeObject(Storage stor, FileOutputStream f) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(f);
+			out.writeObject(stor);
+			out.close();
+		} catch(IOException i){
+			return;
+		}
+	}
+	public Storage DeserializeObject(FileInputStream f) {
+		try{ 
+			ObjectInputStream in = new ObjectInputStream(f);
+			Storage ret = (Storage) in.readObject();
+			in.close();
+			return ret;
+		} catch (IOException i){
+			Storage ret = new Storage();
+			return ret;
+		} catch (ClassNotFoundException c){
+			Storage ret = new Storage();
+			return ret;
+		}
+	}
+}
