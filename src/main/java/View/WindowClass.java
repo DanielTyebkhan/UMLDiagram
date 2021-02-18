c08d188a70324d9332171313d785554f7e73e09f
Sai Lyon Ho <sailyonho@Sais-MacBook-Pro.local>
2021-02-18 09:47:24 -0500

    edited WindowClass and MenuPanel

--- src/main/java/View/WindowClass.java
+++ src/main/java/View/WindowClass.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 
 public class WindowClass extends JPanel{
 
+    //Delete paintComponent when DiagramPanel implemented
     private static final int RECT_X = 20;
     private static final int RECT_Y = RECT_X;
     private static final int RECT_WIDTH = 100;
@@ -25,41 +26,11 @@ public class WindowClass extends JPanel{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
 
-        // //Creating the MenuBar and adding components
-        // JMenuBar mb = new JMenuBar();
-        // JMenu m1 = new JMenu("File");
-        // JMenu m2 = new JMenu("Help");
-        // JMenu m3 = new JMenu("Export");
-        // mb.add(m1);
-        // mb.add(m2);
-        // mb.add(m3);
-        // JMenuItem m11 = new JMenuItem("Open");
-        // JMenuItem m22 = new JMenuItem("Save as");
-        // m1.add(m11);
-        // m1.add(m22);
-
-        // //Creating the panel at bottom and adding components
-        // JPanel panel = new JPanel(); // the panel is not visible in output
-        // JButton send = new JButton("Add Diagram");
-        // JButton reset = new JButton("Delete");
-        // panel.add(send);
-        // panel.add(reset);
-
-        
-
         //Creating main panel displaying the diagrams of paintComponent
         WindowClass paintPanel = new WindowClass();
 
-        //Adding Components to the frame.
-        // frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        // frame.getContentPane().add(BorderLayout.NORTH, mb);
-        
+      
         MenuPanel menu = new MenuPanel(frame);
-        menu.open();
-        menu.saveAs();
-        menu.export();
-        menu.addDiagramButton();
-        menu.deleteDiagramButton();
 
         frame.getContentPane().add(BorderLayout.CENTER, paintPanel);
         frame.setVisible(true);   
