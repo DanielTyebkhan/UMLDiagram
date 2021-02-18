709c726bb2d42d723b69d9f85ac71e1fc83af9cb
Sai Lyon Ho <sailyonho@Sais-MacBook-Pro.local>
2021-02-17 23:56:56 -0500

    connected MenuPanel to WindowClass

---
+++ src/main/java/View/MenuPanel.java
@@ -0,0 +1,47 @@
+package View;
+
+import java.awt.event.*; 
+import java.awt.*; 
+import javax.swing.*;
+
+public class MenuPanel extends JComponent {
+    private JMenuBar menuBar;
+    private JPanel bottomPanelBar;
+    private JFrame frame;
+    
+    public MenuPanel(JFrame frame)
+    {
+        menuBar = new JMenuBar();
+        bottomPanelBar = new JPanel();
+        
+        this.frame = frame;
+        frame.getContentPane().add(BorderLayout.SOUTH, bottomPanelBar);
+        frame.getContentPane().add(BorderLayout.NORTH, menuBar);
+    }
+    
+    public void open()
+    {
+        JMenu m = new JMenu("Open");
+        menuBar.add(m);
+    } 
+    public void export()
+    {
+        JMenu m = new JMenu("Export");
+        menuBar.add(m);
+    }
+    public void saveAs()
+    {
+        JMenu m = new JMenu("Save As");
+        menuBar.add(m);
+    }
+    public void addDiagramButton()
+    {
+        JButton button = new JButton("Add");
+        bottomPanelBar.add(button);
+    }
+    public void deleteDiagramButton()
+    {
+        JButton button = new JButton("Delete");
+        bottomPanelBar.add(button);
+    }
+}
--- src/main/java/View/WindowClass.java
+++ src/main/java/View/WindowClass.java
@@ -6,11 +6,12 @@ import javax.swing.*;
 // import com.apple.eio.FileManager;
 
 public class WindowClass extends JPanel{
+
     private static final int RECT_X = 20;
     private static final int RECT_Y = RECT_X;
     private static final int RECT_WIDTH = 100;
     private static final int RECT_HEIGHT = RECT_WIDTH;
-
+    
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -24,32 +25,42 @@ public class WindowClass extends JPanel{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
 
-        //Creating the MenuBar and adding components
-        JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("File");
-        JMenu m2 = new JMenu("Help");
-        JMenu m3 = new JMenu("Export");
-        mb.add(m1);
-        mb.add(m2);
-        mb.add(m3);
-        JMenuItem m11 = new JMenuItem("Open");
-        JMenuItem m22 = new JMenuItem("Save as");
-        m1.add(m11);
-        m1.add(m22);
+        // //Creating the MenuBar and adding components
+        // JMenuBar mb = new JMenuBar();
+        // JMenu m1 = new JMenu("File");
+        // JMenu m2 = new JMenu("Help");
+        // JMenu m3 = new JMenu("Export");
+        // mb.add(m1);
+        // mb.add(m2);
+        // mb.add(m3);
+        // JMenuItem m11 = new JMenuItem("Open");
+        // JMenuItem m22 = new JMenuItem("Save as");
+        // m1.add(m11);
+        // m1.add(m22);
 
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JButton send = new JButton("Add Diagram");
-        JButton reset = new JButton("Delete");
-        panel.add(send);
-        panel.add(reset);
+        // //Creating the panel at bottom and adding components
+        // JPanel panel = new JPanel(); // the panel is not visible in output
+        // JButton send = new JButton("Add Diagram");
+        // JButton reset = new JButton("Delete");
+        // panel.add(send);
+        // panel.add(reset);
+
+        
 
         //Creating main panel displaying the diagrams of paintComponent
         WindowClass paintPanel = new WindowClass();
 
         //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        // frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        // frame.getContentPane().add(BorderLayout.NORTH, mb);
+        
+        MenuPanel menu = new MenuPanel(frame);
+        menu.open();
+        menu.saveAs();
+        menu.export();
+        menu.addDiagramButton();
+        menu.deleteDiagramButton();
+
         frame.getContentPane().add(BorderLayout.CENTER, paintPanel);
         frame.setVisible(true);   
     }  
