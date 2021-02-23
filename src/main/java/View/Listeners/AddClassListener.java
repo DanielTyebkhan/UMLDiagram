66965a103b61cbb7ff4eb1e77ff513645dc1de18
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-02-23 16:14:34 -0500

    fixed noncompile with listener

--- src/main/java/View/Listeners/AddClassListener.java
+++ src/main/java/View/Listeners/AddClassListener.java
@@ -2,11 +2,24 @@ package View.Listeners;
 
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+import java.awt.Container;
+import java.awt.Point;
+import javax.swing.JOptionPane;
+
+import Document.ObjectClass;
+import Document.Storage;
 
 public class AddClassListener implements MouseListener {
     private static final String CLASS_NAME_PROMPT = "Enter Class Name";
+    
+    private Container container;
+
+    public AddClassListener(Container container) {
+        this.container = container;
+    }
+
     public void mouseClicked(MouseEvent e) {
-       String name = JOptionPane.showInputDialog(this, CLASS_NAME_PROMPT);
+       String name = JOptionPane.showInputDialog(container, CLASS_NAME_PROMPT);
        if (name != null || !name.equals(""))
            Storage.instance.addObject(new ObjectClass(name, new Point(e.getPoint())));
     }
