c087518e63d512be00c9164b5a420a10020c73d4
Sai Lyon Ho <sailyonho@Sais-MacBook-Pro.local>
2021-02-22 21:58:14 -0500

    Refactored WindowClass.java by creating WindowClass() function that creates the frame and calls MenuPanel.java

--- src/main/java/View/WindowClass.java
+++ src/main/java/View/WindowClass.java
@@ -6,33 +6,17 @@ import javax.swing.*;
 // import com.apple.eio.FileManager;
 
 public class WindowClass extends JPanel{
+    private static final int size = 500;
 
-    //Delete paintComponent when DiagramPanel implemented
-    private static final int RECT_X = 20;
-    private static final int RECT_Y = RECT_X;
-    private static final int RECT_WIDTH = 100;
-    private static final int RECT_HEIGHT = RECT_WIDTH;
-    
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        // draw the rectangle here
-        g.drawRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+    public WindowClass() {
+        JFrame frame = new JFrame("Build Your Diagram!");
+        new MenuPanel(frame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(size, size);
+        frame.setVisible(true); 
     }
 
     public static void main(String[] args){
-        //Creating the Frame
-        JFrame frame = new JFrame("Build Your Diagram!");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-
-        //Creating main panel displaying the diagrams of paintComponent
-        WindowClass paintPanel = new WindowClass();
-
-      
-        MenuPanel menu = new MenuPanel(frame);
-
-        frame.getContentPane().add(BorderLayout.CENTER, paintPanel);
-        frame.setVisible(true);   
+        new WindowClass();
     }  
 }
