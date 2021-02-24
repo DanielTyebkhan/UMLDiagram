c4dca7ab9127f3e2a8bc1781fe3b9a2fbc254f93
Anhad <anhadgande7@gmail.com>
2021-02-24 00:00:06 -0500

    changed ObjectClass to implement a jPanel with JLabels instead of using JLists

--- src/main/java/View/ObjectComponent.java
+++ src/main/java/View/ObjectComponent.java
@@ -6,53 +6,43 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.util.ArrayList;
+import java.util.List;
 
 import Document.ObjectClass;
 import Document.Notable;
 import View.Listeners.AddClassListener;
+import View.ArrowDrawer;
+import Document.Arrow;
 
 
-public class ObjectComponent implements ListSelectionListener{
+public class ObjectComponent implements MouseListener {
 	private static final int HEIGHT = 30;
 	private static final int WIDTH  = 10;
 	private ObjectClass obj;
-	private JList list;
-	private DefaultListModel listModel;
-	private JScrollPane listScrollPane;
+	private JPanel panel;
 	JPopupMenu rcmenu;
 	JMenuItem newMethod;
 	JMenuItem newVariable;
 
 	public ObjectComponent(ObjectClass obj) {
 	 	 this.obj = obj;
+	 	 panel = new JPanel();
+	 	 panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+	 	 JLabel name = new JLabel(obj.getName());
+	 	 panel.add(name);
+	 	 panel.add(new JLabel("obj.getName()"));
+	 	 panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+	 	 // panel.addMouseListener(new NewPanelListener());
+
+
 	 	 rcmenu = new JPopupMenu();
 	 	 newMethod = new JMenuItem("New Method");
 	 	 newVariable = new JMenuItem("New Variable");
-         // newMethod.addMouseListener(new AddClassListener());
-         // newVariable.addMouseListener(new AddClassListener());
+         newMethod.addMouseListener(new MethodAddListener(this));
+         newVariable.addMouseListener(new VarAddListener(this));
          rcmenu.add(newMethod);
          rcmenu.add(newVariable);
-         listModel = new DefaultListModel();
-         listModel.addElement(obj.getName());
-         list = new JList(listModel);
-
-         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-         list.setSelectedIndex(0);
-         list.addListSelectionListener(new SharedListSelectionHandler());
-         list.setVisibleRowCount(10);
-         listScrollPane = new JScrollPane(list);
-         list.addMouseListener(new MouseAdapter()
-	     {
-	        public void mousePressed(MouseEvent e)
-	        {
-	            if ( SwingUtilities.isRightMouseButton(e) )
-	            {
-	                rcmenu.show(list, e.getX(), e.getY());
-	            }
-	        }
-	     }); 
 	 }
-
 	/**
 	* Adds a class name to the class diagram
 	*/
@@ -71,43 +61,116 @@ public class ObjectComponent implements ListSelectionListener{
 	public void removeMethod(String method) {
 		obj.removeMethod(new Notable(method));
 	}
+	public void addVariable(String var) {
+		obj.addInstanceVariable(new Notable(var));
+	}
+	public void removeVariable(String var) {
+		obj.removeInstanceVariable(new Notable(var));
+	}
 
-	public void drawShape(JPanel panel) {
+	public void drawShape(JPanel reference, List<ArrowDrawer> arrows) {
+		Dimension dimension = panel.getSize();
+		System.out.println(dimension);
 		for (Notable method : obj.getMethods()) {
-			listModel.addElement(method.getName());
+			panel.add(new JLabel(method.getName()));
+			for (ArrowDrawer arrow : arrows) {
+				if ( arrow.getArrow().getFrom().equals(method)) {
+				// arrow.setFromPosition(point);
+				}
+			}
 		}
 		for (Notable variable : obj.getInstanceVariables()) {
-			listModel.addElement(variable.getName());
+			panel.add(new JLabel(variable.getName()));
+			for (ArrowDrawer arrow : arrows) {
+				if ( arrow.getArrow().getFrom().equals(variable)) {
+				// arrow.setFromPosition(point);
+				}
+			}
 		}
-		list = new JList(listModel);
-		panel.add(list);
+		reference.add(panel);
 		// reference.add(listScrollPane, BorderLayout.CENTER);
 
 	}
-	// public void mouseClicked(MouseEvent e) {
-	// 	if(e.getButton() == MouseEvent.BUTTON3) {
-	// 		rcmenu.show(list, e.getX(), e.getY());
-	// 	}
-	// }
-	// public void mouseEntered(MouseEvent e) {
-
-	// }
-
-	// public void mouseExited(MouseEvent e) {
-
-	// }
-	// public void mousePressed(MouseEvent e) {
-
-	// }
-	// public void mouseReleased(MouseEvent e) {
-
-	// }
-	public void valueChanged(ListSelectionEvent e) {
-			
-		}
-	class SharedListSelectionHandler implements ListSelectionListener { 
-		public void valueChanged(ListSelectionEvent e) {
-
+	public void mouseClicked(MouseEvent e) {
+		if(e.getButton() == MouseEvent.BUTTON3) {
+			rcmenu.show(panel, e.getX(), e.getY());
 		}
 	}
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+	}
+	public void mousePressed(MouseEvent e) {
+
+	}
+	public void mouseReleased(MouseEvent e) {
+
+	}
+    class VarAddListener implements MouseListener {
+        private ObjectComponent instance;
+        JPopupMenu varMenu;
+        JMenuItem varItem;
+        
+        public VarAddListener(ObjectComponent obj) {
+            instance = obj;
+        }
+
+        public void mouseClicked(MouseEvent e)
+        {
+            String newVar = JOptionPane.showInputDialog(panel, "Enter new variable name");
+            instance.addVariable(newVar);
+        }
+        public void mouseEntered(MouseEvent e)
+        {
+
+        }
+        public void mouseExited(MouseEvent e)
+        {
+
+        }
+        public void mousePressed(MouseEvent e)
+        {
+
+        }
+
+        public void mouseReleased(MouseEvent e)
+        {
+
+        }
+    }
+    class MethodAddListener implements MouseListener {
+        private ObjectComponent instance;
+        JPopupMenu varMenu;
+        JMenuItem varItem;
+        
+        public MethodAddListener(ObjectComponent obj) {
+            instance = obj;
+        }
+
+        public void mouseClicked(MouseEvent e)
+        {
+            String newMethod = JOptionPane.showInputDialog(panel, "Enter new method name");
+            instance.addMethod(newMethod);
+        }
+        public void mouseEntered(MouseEvent e)
+        {
+
+        }
+        public void mouseExited(MouseEvent e)
+        {
+
+        }
+        public void mousePressed(MouseEvent e)
+        {
+
+        }
+
+        public void mouseReleased(MouseEvent e)
+        {
+
+        }
+    }
 }
