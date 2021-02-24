245b07effd996797ea74673a1db6730bd1ab5152
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-02-24 15:16:55 -0500

    Implemented listener to add methods

--- src/main/java/View/Listeners/MethodAddListener.java
+++ src/main/java/View/Listeners/MethodAddListener.java
@@ -7,35 +7,38 @@ import javax.swing.event.*;
 
 import View.ObjectComponent;
 
+/**
+ * A listener to add methods to an object
+ * @author Daniel Tyebkhan
+ * @author Anhad Gande
+ */
 public class MethodAddListener implements MouseListener {
-        private ObjectComponent instance;
-        JPopupMenu varMenu;
-        JMenuItem varItem;
-        
-        public MethodAddListener(ObjectComponent obj) {
-            instance = obj;
-        }
+    private static final String ENTER_NAME = "Enter method name";
+    private ObjectComponent object;
 
-        public void mouseClicked(MouseEvent e)
-        {
-            // String newMethod = JOptionPane.showInputDialog(panel, "Enter new method name");
-            // instance.addMethod(newMethod);
-        }
-        public void mouseEntered(MouseEvent e)
-        {
-
-        }
-        public void mouseExited(MouseEvent e)
-        {
-
-        }
-        public void mousePressed(MouseEvent e)
-        {
-
-        }
-
-        public void mouseReleased(MouseEvent e)
-        {
-
-        }
+    public MethodAddListener(ObjectComponent object, JPanel parent) {
+        this.object = object;
     }
+
+    public void mouseClicked(MouseEvent e) {
+        String newMethod = JOptionPane.showInputDialog(e.getComponent(), ENTER_NAME);
+        object.addMethod(newMethod);
+    }
+    public void mouseEntered(MouseEvent e)
+    {
+
+    }
+    public void mouseExited(MouseEvent e)
+    {
+
+    }
+    public void mousePressed(MouseEvent e)
+    {
+
+    }
+
+    public void mouseReleased(MouseEvent e)
+    {
+
+    }
+}
