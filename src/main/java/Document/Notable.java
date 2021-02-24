606a803b5e529e943078b04bc9f88287b41a785d
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-02-23 23:14:56 -0500

    Added Notable javadocs

--- src/main/java/Document/Notable.java
+++ src/main/java/Document/Notable.java
@@ -1,34 +1,69 @@
 package Document;
 
+/**
+ * Represents an object with a name and an attachable note
+ * @author Daniel Tyebkhan
+ */
 public class Notable {
     String name,
            note;
 
+    /**
+     * Constructs a notable with a name and note
+     * @param name the name
+     * @param note the note
+     */
     public Notable(String name, String note) {
         this.name = name;
         this.note = note;
     }
 
+    /**
+     * Constructs a notable with a name
+     * @param name the name 
+     */
     public Notable(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the name
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name
+     * @param name the new name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the note
+     * @param note the new note
+     */
     public void setNote(String note) {
         this.note = note;
     }
 
+    /**
+     * Gets the note
+     * @return the note
+     */
     public String getNote() {
         return note;
     }
 
+    /**
+     * Checks if two notables are equal
+     * Notables are equal if they have the same name and note
+     * @param other the object to compare to
+     * @return true if this equals other else false
+     */
     @Override
     public boolean equals(Object other) {
         if (other instanceof Notable) {
