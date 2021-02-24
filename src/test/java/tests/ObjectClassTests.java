da0f0d7a1aec44729429a5d50ee8687a70f2d1bf
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-02-23 23:35:53 -0500

    Added ObjectClass tests

--- src/test/java/tests/ObjectClassTests.java
+++ src/test/java/tests/ObjectClassTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.Before;
@@ -10,6 +11,10 @@ import org.junit.runners.JUnit4;
 import java.awt.Point;
 import Document.ObjectClass;
 
+/**
+ * Tests fo the ObjectClass class
+ * @author Daniel Tyebkhan
+ */
 @RunWith(JUnit4.class)
 public class ObjectClassTests {
     private ObjectClass testObj;
@@ -23,6 +28,20 @@ public class ObjectClassTests {
     public void getName() {
         assertEquals("Constructor should initialize name", testObj.getName(), "test");
     }
+
+    @Test
+    public void addChild() {
+        ObjectClass child = new ObjectClass("child", new Point(0,0));
+        testObj.addChild(new ObjectClass("child", new Point(0,0)));
+        assertTrue("Adding a child should add it", testObj.getChildren().contains(child));
+    }
+
+    @Test
+    public void setName() {
+        testObj.setName("TestName");
+        assertEquals("Setting name should change name", "TestName", testObj.getName());
+    }
+
 }
 
 
