2d7104705a43e6fec9774c74ab5dcef42bfcfd2d
Lawson Wheatley <wheatlel@union.edu>
2021-02-24 22:23:01 -0500

    Added

--- src/main/java/View/Listeners/SaveFileListener.java
+++ src/main/java/View/Listeners/SaveFileListener.java
@@ -2,21 +2,26 @@ package View.Listeners;
 
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Point;
 import javax.swing.JOptionPane;
 
 import Document.ObjectClass;
 import Document.Storage;
+import Filemanagement.Filechoose;
 
 /**
  * @author Sai Lyon Ho 
  */
 public class SaveFileListener implements MouseListener {
+    private Filechoose fchoose = new Filechoose();
 
     public void mouseClicked(MouseEvent e) 
     {
         if (e.getButton() == MouseEvent.BUTTON1) 
-        System.out.println("Clicked Save!");
+		fchoose.SaveFile();
+        	System.out.println("Clicked Save!");
     }
     public void mouseEntered(MouseEvent e)
     {
@@ -33,6 +38,5 @@ public class SaveFileListener implements MouseListener {
 
     public void mouseReleased(MouseEvent e) {
     }
-
 }
 
