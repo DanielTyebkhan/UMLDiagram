d96678f8b07bc3a1d4dc0b5249fec599ec4e5084
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-03-06 14:31:30 -0500

    Added Method class

---
+++ src/main/java/Document/Method.java
@@ -0,0 +1,28 @@
+package Document;
+
+import java.util.ArrayList;
+
+public class Method extends Notable {
+	private ArrayList<String> parameters;
+
+	public Method(String name, ArrayList<String> parameters) {
+		super(name);
+		this.parameters = new ArrayList(parameters);
+	} 
+
+	public ArrayList<String> getParameters() {
+		return new ArrayList(parameters);
+	}
+
+	public void addParameter(String name, int index) {
+		parameters.add(index, name);
+	}
+
+	public void removeParameter(int index) {
+		parameters.remove(index);
+	}
+
+	public void renameParameter(String newName, int index) {
+		parameters.set(index, newName);
+	}
+}
