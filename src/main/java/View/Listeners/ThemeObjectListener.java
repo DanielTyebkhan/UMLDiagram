8c158ba3f015194575add22756187ec3c4ca1b8e
Sai Lyon Ho <sailyonho@Sais-MacBook-Pro.local>
2021-03-07 14:05:15 -0500

    added ThemeObjectListener which allows user to interact with ThemeObject frame

---
+++ src/main/java/View/Listeners/ThemeObjectListener.java
@@ -0,0 +1,28 @@
+package View.Listeners;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import View.ThemeObject;
+
+
+
+/**
+ *  
+ * @author Sai Lyon Ho
+ */
+public class ThemeObjectListener implements ActionListener {
+
+	/**
+	 * actionPerformed
+	 * @param ActionEvent e
+	 */
+	public void actionPerformed(ActionEvent e) {
+        System.out.println("Clicked Open Theme!");
+		new ThemeObject();
+	}
+}
+
+
