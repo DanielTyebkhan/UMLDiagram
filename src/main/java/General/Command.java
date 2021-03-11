566715a41daea322df781a9b0d719733d187b5cf
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-03-11 16:40:50 -0500

    Added command javadocs

--- src/main/java/General/Command.java
+++ src/main/java/General/Command.java
@@ -4,7 +4,14 @@ package General;
  * Defines a command
  */
 public interface Command {
+
+	/**
+	 * Executes the command
+	 */
 	public void execute();
 
+	/**
+	 * Undoes the command
+	 */
 	public void unexecute();
 }
